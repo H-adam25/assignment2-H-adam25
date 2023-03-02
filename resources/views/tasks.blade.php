ab6025c33ed056a481d7c5716a72997483071ccf
H-adam25 <hamza.adam@ontariotechu.net>
2023-03-01 22:35:19 -0500

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -37,7 +37,11 @@
                             <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
                                 <div>
                                     <p class="">{{ $task->name }}</p>
-                                    <p class="text-sm text-gray-500">Due Date: <time datetime="2020-10-04">{{ $task->due_date }}</time></p>
+                                    @if ($task->completed_at)
+                                    <p class="text-sm text-gray-500">Completed at: <time datetime="{{ $task->completed_at }}">{{ $task->completed_at->format('jS F Y \\a\\t h:i:s A') }}</time></p>
+                                    @else
+                                    <p class="text-sm text-gray-500">Due Date: <time datetime="{{ $task->due_date }}">{{ $task->due_date }}</time></p>
+                                    @endif
                                 </div>
                                 <div class="whitespace-nowrap text-right text-sm text-gray-500">
                                     <a href="{{ route('tasks.show', $task->id) }}"
@@ -47,15 +51,14 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                         </svg>
                                     </a>
-                                    <button type="button" @click="$refs['task-{{ $task->id }}'].submit();"
-                                        class="inline-flex items-center justify-center rounded-md border border-transparent bg-red-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                        </svg>
-                                    </button>
-                                    <form x-ref="task-{{ $task->id }}" method="POST" action="{{ route('tasks.destroy', ['task' => $task]) }}">
+                                    <form method="POST" action="{{ route('tasks.destroy', ['task' => $task]) }}" class="inline-flex items-center justify-center">
                                         @csrf
                                         @method('DELETE')
+                                        <button type="submit" class="rounded-md border border-transparent bg-red-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                            </svg>
+                                        </button>
                                     </form>
                                 </div>
                             </div>
